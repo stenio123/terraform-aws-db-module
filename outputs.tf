38083405a11351e9c31fbcd740abb4f896497638
Your Name <you@example.com>
2021-05-10 16:05:00 -0500

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "db_ip_addr" {
   value = aws_db_instance.default.address
 }
 
-output "aws_secret_id" {
-  value = aws_secretsmanager_secret.example.id
-}
-
-output "aws_secret_arn" {
-  value = aws_secretsmanager_secret.example.arn
+output "vault_secret_path" {
+  value = "secret/db_${random_string.random_secret_name.result}"
 }
